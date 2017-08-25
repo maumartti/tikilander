d41b16ae1a0d158ef8fffb8bfc05208e9b11299d
maumartti <maumartti@gmail.com>
2017-08-24 21:59:19 -0300

    nuevo tikilander

--- js/code.js
+++ js/code.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
-	//slider
-	$(function(){
-	    $('#slider li:gt(0)').hide();
-	    setInterval(function(){
-	      $('#slider li:first-child').fadeOut(2000)
-	         .next('li').fadeIn(2000)
-	         .end().appendTo('#slider');
-	     },6000);
-	});
+
+
+  $(".tiki").click(function() {
+  	$(".tiki").css('border',"1px solid #83B541");
+  	$(".tiki").css('border-right',"none");
+  	$(".tiki").css('text-shadow',"none"); 
+  	$(".tiki").css('background',"#fff");
+  	$(".tiki").find('.fa-tree').css('color','#83B541');
+    /*$(this).css('border', "solid 0px #88b161");*/
+    $(this).css('text-shadow','1px 2px 4px #fff');
+    $(this).find('.fa-tree').css('color','rgb(29, 138, 26)');
+
+    $(this).fadeTo('slow', 0.7, function()
+	{
+    $(this).css('background-image', 'url(images/f2.png)');
+    $(this).css('background-repeat', 'no-repeat');
+    $(this).css('background-size', '100%');
+	}).delay(1000).fadeTo('slow', 1);
+
+    document.getElementById('sound').play();
+     //$('#sound').play();
+  	
+
+  });
 
 });
+
