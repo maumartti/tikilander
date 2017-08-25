573ec9328907d668ba166cd97ea45a76cee5b919
maumartti <maumartti@gmail.com>
2017-08-25 01:46:13 -0300

    agrego boton

--- js/code.js
+++ js/code.js
@@ -1,25 +1,20 @@
 $(document).ready(function(){
 
 
-  $(".tiki").click(function() {
-  	$(".tiki").css('border',"1px solid #83B541");
-  	$(".tiki").css('border-right',"none");
-  	$(".tiki").css('text-shadow',"none"); 
-  	$(".tiki").css('background',"#fff");
-  	$(".tiki").find('.fa-tree').css('color','#83B541');
-    /*$(this).css('border', "solid 0px #88b161");*/
-    $(this).css('text-shadow','1px 2px 4px #fff');
-    $(this).find('.fa-tree').css('color','rgb(29, 138, 26)');
-
-    $(this).fadeTo('slow', 0.7, function()
-	{
-    $(this).css('background-image', 'url(images/f2.png)');
-    $(this).css('background-repeat', 'no-repeat');
-    $(this).css('background-size', '100%');
+  $(".tiki").click(function() {	
+	
+	$('.tiki').removeClass('tikiClick'); 
+	$('.tiki').find('.fa-tree').removeClass('fa-tree-click');
+	$('.tiki').find('.btn').css('display','none');
+	$(this).find('.fa-tree').addClass('fa-tree-click');
+    
+    $(this).fadeTo('slow', 0.7, function(){
+    	$(this).toggleClass('tikiClick');
+		$(this).find('.btn').show();
 	}).delay(1000).fadeTo('slow', 1);
-
+    
     document.getElementById('sound').play();
-     //$('#sound').play();
+  	 
   	
 
   });
